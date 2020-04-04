0d26d265dd1b8cf7fcc87c0671e1279e1ac65b10
deanjheikkila <deanjheikkila@gmail.com>
2020-04-03 17:07:17 -0700

    set up initial files in client

---
+++ client/src/content/Content.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// import pages here
+// import Profile from './pages/profile/Profile';
+
+
+export default function Content(props) {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Poems} />
+                <Route path="/profile/edit" render={() => <EditProfile user={props.user} />} />
+                <Route path="/profile" render={() => <Profile user={props.user} />} />
+                <Route path="/results" component={Results} />
+                <Route path="/results/:id" component={ShowResult} />
+                <Route path="/poems/new" render={() => <NewPoem user={props.user} />} />
+                <Route path="/poems/:id" render={() => <ShowPoem user={props.user} />} />
+                <Route path="/auth/login" render={() => <Login user={props.user} updateUser={props.updateUser} /> } />
+                <Route path="/auth/signup" render={() => <Signup user={props.user} updateUser={props.updateUser} /> } />
+            </Switch>
+        </div>
+    )
+
+}
