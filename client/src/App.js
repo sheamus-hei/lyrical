0243a42ef1e9fff8c4b4bfd400a36e75d56b88f3
deanjheikkila <deanjheikkila@gmail.com>
2020-04-30 12:59:54 -0700

    preparing for redeploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 // import jwtDecode from 'jwt-decode';
 
 import Header from './wrappers/Header'
