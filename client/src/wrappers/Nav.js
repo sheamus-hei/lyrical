2eb5bd70a211de12e0ac2ca1577bd683f188507f
deanjheikkila <deanjheikkila@gmail.com>
2020-05-01 17:59:27 -0700

    changed nav link

--- client/src/wrappers/Nav.js
+++ client/src/wrappers/Nav.js
@@ -16,7 +16,7 @@ export default function Nav(props) {
                 <Link to="/results" className="App-link">Browse Songs</Link>
             </li>
             <li>
-                <a href="https://github.com/dean-hei/genius-poetry-corner" target="_blank" className="App-link">About</a>
+                <a href="https://github.com/erik-hei/genius-poetry-corner" target="_blank" rel="noopener noreferrer" className="App-link">About</a>
             </li>
         </ul>
     )
