3a1e7db0b11a4f4c9bae7870739b7ee0f1c7255b
deanjheikkila <deanjheikkila@gmail.com>
2020-05-13 20:38:17 -0700

    merge conflicts in readme after revert

---
+++ testAPI.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# source: https://bigishdata.com/2016/09/27/getting-song-lyrics-from-geniuss-api-scraping/
+# I borrowed this from a blog to data scrape lyrics because the genius API doesn't include lyrics
+import requests
+from bs4 import BeautifulSoup
+
+def lyrics_from_url(page_url):
+  page = requests.get("https://genius.com/" + page_url)
+  html = BeautifulSoup(page.text, "html.parser")
+  #remove script tags that they put in the middle of the lyrics
+  [h.extract() for h in html('script')]
+  #at least Genius is nice and has a tag called 'lyrics'!
+  lyrics = html.find("div", class_="lyrics").get_text() #updated css where the lyrics are based in HTML
+  lyrics = lyrics.split('\n')
+  return lyrics
+
+if __name__ == "__main__":
+  search_url = "Kendrick-lamar-humble-lyrics"
+  print(lyrics_from_url(search_url))
